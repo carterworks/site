a2f464efdaf10f698a85f1e59190f4f9a14049d0
Carter McBride <junk@carter.works>
2020-09-22 19:33:16 -0600

    copy carterworks/the-bullpen and adapt to carter.works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
-// tailwind.config.js
 module.exports = {
-  purge: {
-    content: ["./src/**/*.html"],
-    options: {},
-  },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './public/**/*.html',
+  ],
   theme: {
     extend: {
-      colors: {},
+      fontFamily: {
+        mono: [
+          'JetBrainsMono',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'Liberation Mono',
+          'Courier New',
+          'monospace',
+        ],
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': {
+            opacity: 1,
+          },
+          '50%': {
+            opacity: 0,
+          },
+        },
+      },
+    },
+    animation: {
+      blink: 'blink 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
     },
   },
   variants: {},
