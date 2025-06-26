e3da05f9c73091033a197fa991b491badfd28a0b
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-06-26 15:15:25 -0600

    Write a blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,10 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
 	type: "content",
 	schema: z.object({
-		title: z.string(),
+		title: z.string().optional(),
 		pubDate: z.date(),
-		description: z.string(),
-		tags: z.array(z.string()),
+		description: z.string().optional(),
+		tags: z.array(z.string()).optional(),
 	}),
 });
 export const collections = { blog };
