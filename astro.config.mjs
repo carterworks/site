aa4e1019a46f0ece430130bd1ddbed0385eab22b
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-06-26 15:33:19 -0600

    Initialize the 2025 theme (#314)
    * Update wrangler
    
    * Update many deps
    
    * Update biome
    
    * Update typescript
    
    * Add base body styles
    
    * Remove astro-icon and tailwind
    
    * Iterate
    
    * Write a blog post
    
    * More
    
    * 6.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@tailwindcss/vite";
-import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://carter.works",
-	integrations: [sitemap(), tailwind(), icon()],
+	integrations: [sitemap()],
 	output: "static",
-	vite: {
-		plugins: [tailwind()],
-	},
 });
